4fcb96a2cd89cd4473f810967c7fc371d0218daf
raystatic <rahul9650ray@gmail.com>
2019-09-01 19:37:48 +0530

    CORS error handling done!

--- app.js
+++ app.js
@@ -10,6 +10,25 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
+// Handle CORS Errors
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-with, Content-Type, Accept, Authorization"
+    );
+
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods", 'PUT, POST, PATCH, DELETE, GET');
+        res.status(200).json({});
+    }
+    next();
+
+});
+
+
+//Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
